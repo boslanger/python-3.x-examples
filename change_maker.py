8e8db676dca5746e2323b6a3d5bcc33f8caf0fe5
boslanger <43486210+boslanger@users.noreply.github.com>
2018-11-06 08:30:10 -0600

    Update change_maker.py

--- change_maker.py
+++ change_maker.py
@@ -1,13 +1,9 @@
 """
-Code that gives change.
-Found a way around the float approximate problem.
+jbollinger@yahoo.com
+6 NOV 2018
+Change Maker
+Code that outputs amount of bills and coins.
 """
-
-total = 342.59
-amount_paid = 500
-
-change = amount_paid - total
-change = round(change, 2)
 
 
 def count_change(change):
@@ -19,7 +15,6 @@ def count_change(change):
 	five = 0
 	single = 0
 	if type(change) == str:
-		"""checks input to be int or float only."""
 		print("You must use only integers or floats.")
 	if type(change) != str:
 
@@ -44,24 +39,49 @@ def count_change(change):
 
 		# print("this should be zero or change: ", change) # for testing
 		"""This is for counting out the bills."""
-		if hundred > 0:	print("Hundreds:", hundred)
-		if twenty > 0:	print("Twenties:", twenty)
-		if ten > 0:	print("Tens:", ten)
-		if five > 0: print("Fives:", five)
-		if single > 0: print("Dollars:", single)
+		if hundred > 0:
+			print("Hundreds:", hundred)
+		if twenty > 0:
+			print("Twenties:", twenty)
+		if ten > 0:
+			print("Tens:", ten)
+		if five > 0:
+			print("Fives:", five)
+		if single > 0:
+			print("Dollars:", single)
 
 		"""This is for coin change only."""
 		change = round(change, 2) * 100
-		if change >= 25: print("Quarters:", int(change//25))
+		if change >= 25:
+			print("Quarters:", int(change//25))
 		change = change%25
-		if change >= 10: print("Dimes:", int(change//10))
+		if change >= 10:
+			print("Dimes:", int(change//10))
 		change = change%10
-		if change >= 5: print("Nickles:", int(change//5))
+		if change >= 5:
+			print("Nickles:", int(change//5))
 		change = change%5
-		if change > 0: print("Pennies:", int(change//1))
+		if change > 0:
+			print("Pennies:", int(change//1))
 
 
 
-count_change(change)
-count_change(401)
+"""These simply check for functionality."""
+
+total = 342.59
+amount_paid = 500
+difference = amount_paid - total
+difference = round(difference, 2)
+
+print("\niteration 1 'var'")
+count_change(difference) # good
+print("\niteration 2 'int(410):")
+count_change(401) # good
+print("\niteration 3 'str(asdf)'")
+count_change('asdf') # good
+print("\niteration 4 'float(410.02)'")
+count_change(483.42) # good
+print("\niteration 5 'Nonsense'")
+x = input("nonsense") # good
+count_change(x)
 
